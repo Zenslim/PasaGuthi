ebd20f5c9feb717763035e237aff07d4ec50de75
Zenslim <zenslim2010@gmail.com>
2025-05-07 17:37:38 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,25 @@
-// next.config.js
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in dev mode
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['pasaguthi.org'], // allow optimized use of remote images
+    domains: ['images.unsplash.com', 'pbs.twimg.com'],
   },
-  // Optional: redirects or rewrites can be added here
   async rewrites() {
     return [
       {
         source: '/__/auth/handler',
-        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/handler',
+        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/handler'
       },
       {
         source: '/__/auth/iframe',
-        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/iframe',
+        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/iframe'
       },
       {
         source: '/__/auth/iframe.js',
-        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/iframe.js',
+        destination: 'https://pasaguthi-fc098.firebaseapp.com/__/auth/iframe.js'
       }
     ];
-  },
-});
+  }
+};
+
+module.exports = nextConfig;
