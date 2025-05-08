797158c6da08ca944fb1fa5da4ec140cf79223b4
Zenslim <zenslim2010@gmail.com>
2025-05-08 15:40:05 -0400

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,5 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
