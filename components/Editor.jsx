4c86ff5554066d1f3eb632dbf3edad57bf0d22d8
Zenslim <zenslim2010@gmail.com>
2025-09-14 11:16:39 +0545

    Update Editor.jsx

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,1 +1,33 @@
+import { useEffect, useRef } from 'react'
 
+
+export default function Editor({ initialData, onChange }) {
+const ref = useRef(null)
+
+
+useEffect(() => {
+let editor
+;(async () => {
+const EditorJS = (await import('@editorjs/editorjs')).default
+const Header = (await import('@editorjs/header')).default
+const List = (await import('@editorjs/list')).default
+const ImageTool = (await import('@editorjs/image')).default
+
+
+editor = new EditorJS({
+holder: 'editorjs',
+placeholder: '✍️ Whisper to the forest…',
+tools: { header: Header, list: List, image: ImageTool },
+data: initialData || { blocks: [] },
+onChange: async () => {
+const data = await editor.save()
+onChange?.(data)
+}
+})
+})()
+return () => { editor && editor.destroy?.() }
+}, [])
+
+
+return <div id="editorjs" ref={ref} className="min-h-[300px] p-4 bg-zinc-900 rounded-2xl text-white" />
+}
